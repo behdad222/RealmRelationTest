eb7f90426b0539d843085038310835d53c8b807c
behdad222 <behdad.222@gmail.com>
2015-04-17 12:19:06 +0430

    start more activity with click

--- app/src/main/java/behdad222/realmrelationtest/View/Activity/MainActivity.java
+++ app/src/main/java/behdad222/realmrelationtest/View/Activity/MainActivity.java
@@ -1,15 +1,46 @@
 package behdad222.realmrelationtest.View.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import behdad222.realmrelationtest.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button user;
+    private Button category;
+    private Button product;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = (Button) findViewById(R.id.user);
+        category = (Button) findViewById(R.id.category);
+        product = (Button) findViewById(R.id.product);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.user:
+                i = new Intent (this, UserActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.category:
+                i = new Intent (this, UserActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.product:
+                i = new Intent (this, UserActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
